a7ad41a0a64f37f5840aafd9d9393298f1db954c
Helen Tan <helentanmz0909@gmail.com>
2022-09-26 16:39:58 +0800

    Create Jobs Controller

---
+++ controllers/jobsController.js
@@ -0,0 +1,10 @@
+// Jobs Controller contains all the methods for the Jobs Router
+
+// Get all jobs 
+// GET /api/v1/jobs
+exports.getJobs = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: 'This route will display all jobs in future.'
+    })
+}
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/jobs', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'This route will display all jobs in future.'
-    })
-})
+// Importing Jobs Controller methods
+const { getJobs } = require('../controllers/jobsController');
+
+router.route('/jobs').get(getJobs)
 
 module.exports = router;
